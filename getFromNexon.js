2d1e82196302edee1feb19fbda4f086f64cf32be
Syoongy <mastersyong@gmail.com>
2019-03-16 02:13:03 +0800

    u

--- getFromNexon.js
+++ getFromNexon.js
@@ -7,4 +7,7 @@ async function getData() {
   console.log(data.data);
 }
 
+// Squad builder url
+//http://fifaonline4.nexon.com/DataCenter/SquadMakerPlayerList?n1Confederation=&n4LeagueId=&strSeason=&strPosition=&n4TeamId=&n4NationId=&n1Favorit=0&strOrderby=overallrating%20descending&strPlayerName=&n4OvrMin=40&n4OvrMax=150&n4SalaryMin=4&n4SalaryMax=34&rd=0.5053650015652047
+
 getData();
